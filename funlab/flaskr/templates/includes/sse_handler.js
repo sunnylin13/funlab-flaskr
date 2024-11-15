5c6e3ed10776b137c205583fffb9f4d1d8c9c672
Sunny Lin <sunnylin@mirle.com.tw>
2024-11-15 13:51:24 +0800

    fix: add heartbeat handling

--- funlab/flaskr/templates/includes/sse_handler.js
+++ funlab/flaskr/templates/includes/sse_handler.js
@@ -3,7 +3,13 @@
 function subscribeToEvent(eventType, renderFunction) {
     const eventSource = new EventSource(`/sse/${eventType}`);
     eventSource.onmessage = function(event) {
+        // Check if the message is a heartbeat
+        if (event.data === "heartbeat") {
+            // console.log("Received heartbeat");
+            return; // Ignore heartbeat messages
+        }
         const data = JSON.parse(event.data);
+        console.log("Received notitication:", data);
         renderFunction(data, eventType);
     };
 
